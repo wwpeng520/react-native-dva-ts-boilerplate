8e5d3ff08eeccac27148323789b8777539c77ca0
wwpeng520 <wwpeng520@gmail.com>
2018-07-31 18:09:40 +0800

    add module decare

---
+++ src/types/modules.d.ts
@@ -0,0 +1,11 @@
+/* 
+    Application Module Definitions
+    ---------------------------------------------------------------------------
+    The following modules do not have type declarations available. 
+    Definitions need to be added in here as required.
+
+    Adding definitions
+    ---------------------------------------------------------------------------
+*/ 
+
+declare module "react-native-simple-store"
